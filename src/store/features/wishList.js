7238c79a5fd0e6df0238b4ecf10c8c370d21717c
Nguyen Minh Hieu <66929613+nguyenminhhieu531@users.noreply.github.com>
2025-02-27 21:16:59 +0700

    Update wishList.js

--- src/store/features/wishList.js
+++ src/store/features/wishList.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-    wishList: [],
+    wishList: JSON.parse(localStorage.getItem("wishListStorage")) || [],
 };
 
 export const wishSlice = createSlice({
@@ -12,15 +12,17 @@ export const wishSlice = createSlice({
             if (!productExists) {
                 console.log("Adding product:", action.payload); // Log sản phẩm được thêm
                 state.wishList.push(action.payload);
+                // Lưu vào localStorage
+                localStorage.setItem("wishListStorage", JSON.stringify(state.wishList))
             }
         },
         removeProductFromWishList: (state, action) => {
             console.log("Removing product ID:", action.payload); // Log sản phẩm bị xóa
             state.wishList = state.wishList.filter((item) => item.id !== action.payload);
+            localStorage.setItem("wishListStorage", JSON.stringify(state.wishList));
         },
     },
 });
 
-
-export const { addProductToWishList, removeProductFromWishList } = wishSlice.actions
-export default wishSlice.reducer
+export const { addProductToWishList, removeProductFromWishList } = wishSlice.actions;
+export default wishSlice.reducer;
