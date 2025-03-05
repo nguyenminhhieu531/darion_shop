786ded5435758ceaf9cdaa41cfc0ff5f2b9a3a8b
Nguyen Minh Hieu <66929613+nguyenminhhieu531@users.noreply.github.com>
2025-03-05 10:34:21 +0700

    Update SearchPage.jsx

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -16,6 +16,14 @@ function SearchPage(props) {
                 productSearch.length > 0 ?
                     <section className="pb-12 bg-gray">
                         <div div className="container" >
+                            <ul className="flex gap-2 items-center py-4">
+                                <li>
+                                    <Link to={"/"} className="text-sm"><i className="icon fa fa-home" /> /</Link>
+                                </li>
+                                <li>
+                                    <span className="text-sm">Search</span>
+                                </li>
+                            </ul>
                             <div className="lg:grid grid-cols-4">
                                 <div className="col-span-4 mt-6 lg:mt-6">
                                     <div className="py-2 px-3 border rounded-full cursor-pointer w-max">
