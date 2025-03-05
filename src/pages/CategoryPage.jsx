d630d0be78db3635c7a63512f8d706cae81ec693
Nguyen Minh Hieu <66929613+nguyenminhhieu531@users.noreply.github.com>
2025-03-05 10:36:48 +0700

    Update CategoryPage.jsx

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -28,6 +28,17 @@ function CategoryPage(props) {
     return (
         <section className=" pb-12 bg-gray">
             <div className="container">
+                <ul className="flex gap-2 items-center py-4">
+                    <li>
+                        <Link to={"/"} className="text-sm"><i className="icon fa fa-home" /> /</Link>
+                    </li>
+                    <li>
+                        <span className="text-sm">Category /</span>
+                    </li>
+                    <li>
+                        <span className="text-sm">{categoryName}</span>
+                    </li>
+                </ul>
                 <div className="lg:grid grid-cols-4">
                     <div className="col-span-4 mt-6 lg:mt-6">
                         <div className="py-2 px-3 border rounded-full cursor-pointer w-max">
