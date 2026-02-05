cdb8c24e97a02ad9d15f01b948ccca73bccff934
HIEU NGUYEN <66929613+nguyenminhhieu531@users.noreply.github.com>
2026-02-05 09:48:47 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,25 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routers/routers'
 import toast, { Toaster } from 'react-hot-toast'
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
-import clearCart from '../store/features/cartSlice';
+
 
 function App() {
-  const dispatch = useDispatch();
+
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const payment = params.get('payment');
 
     if (payment === 'success') {
-      // ✅ XÓA REDUX TRƯỚC
-      dispatch(clearCart());
-
-      // ✅ XÓA LOCALSTORAGE
-      localStorage.removeItem('cartItemsStorage');
-
-      toast.success('Thanh toán thành công 🎉');
-
-      // ✅ XÓA QUERY
-      window.history.replaceState({}, document.title, '/order');
+      localStorage.removeItem("cartItemsStorage");
+      toast.success('Thanh toán thành công');
+      window.history.replaceState({}, '', '/order');
     }
 
     if (payment === 'fail') {
       toast.error('Thanh toán thất bại ❌');
-      window.history.replaceState({}, document.title, '/order');
+      window.history.replaceState({}, '', '/order');
     }
   }, []);
-
   return (
     <>
       <Toaster
