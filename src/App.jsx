d4259e42f4a802e0ac47c420b453c92f73863a44
HIEU NGUYEN <66929613+nguyenminhhieu531@users.noreply.github.com>
2026-02-05 11:26:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import './App.css'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/routers'
-import { Toaster, toast } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
+import { useEffect } from 'react'
 
 function App() {
-          useEffect(() => {
-                const params = new URLSearchParams(window.location.search);
-                const payment = params.get("payment");
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const payment = params.get("payment");
 
-                if (payment === "success") {
-                    toast.success("🎉 Thanh toán thành công!");
-                }
+    if (payment === "success") {
+      toast.success("🎉 Thanh toán thành công!");
+    }
 
-                if (payment === "fail") {
-                    toast.error("❌ Thanh toán thất bại!");
-                }
-            }, []);
+    if (payment === "fail") {
+      toast.error("❌ Thanh toán thất bại!");
+    }
+  }, []);
   return (
     <>
       <Toaster
